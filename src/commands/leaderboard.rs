b37ea2f6b54985c472ee38348e8d1237207d7ac1
Rémi Ait-Younes <remi@ait-younes.fr>
2023-11-23 22:48:33 +0100

    fix max leaderboard lenght

--- src/commands/leaderboard.rs
+++ src/commands/leaderboard.rs
@@ -1,13 +1,13 @@
 use crate::{Context, Error};
 
 
-/// Get the top 20 players with the most game won
+/// Get the top 15 players with the most game won
 #[poise::command(slash_command, prefix_command)]
 pub async fn leaderboards(
     ctx: Context<'_>,
 ) -> Result<(), Error> {
     let conn = &ctx.data().0;
-    let rows = sqlx::query!("SELECT * FROM user_stats ORDER BY game_won DESC LIMIT 20").fetch_all(conn).await?;
+    let rows = sqlx::query!("SELECT * FROM user_stats ORDER BY game_won DESC LIMIT 15").fetch_all(conn).await?;
     let mut leader_str = String::new();
 
     for (id, row) in rows.iter().enumerate() {
@@ -15,7 +15,7 @@ pub async fn leaderboards(
     }
 
     ctx.send(|b| b.embed(|e| {
-        e.title("Top 20")
+        e.title("Top 15")
         .field("", leader_str, false)
     }).allowed_mentions(|m| m.empty_users())).await?;
     Ok(())
