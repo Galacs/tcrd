00f24473412b6f7885aca3d5520829a8bd46f588
Rémi Ait-Younes <remi@ait-younes.fr>
2023-12-02 13:56:21 +0100

    profile cmd better name formatting

--- src/commands/profile.rs
+++ src/commands/profile.rs
@@ -10,12 +10,16 @@ pub async fn profile(
     let conn = &ctx.data().0;
     let user = player.unwrap_or(ctx.author().clone());
     let user_id = user.id.0 as i64;
+    let username = match user.discriminator {
+        0000 => user.name,
+        _ => user.tag(),
+    };
     crate::create_user::exists_or_create_user(user_id, conn).await?;
     let user_id = user_id.to_string();
     let stats = sqlx::query!("SELECT user_stats.game_won,user_stats.game_lost,balances.balance FROM user_stats INNER JOIN balances ON user_stats.user_id=balances.user_id WHERE user_stats.user_id=$1", user_id).fetch_one(conn).await?;
     let cards_number = sqlx::query!("SELECT count(user_id) AS count FROM users_cards WHERE user_id=$1", user_id).fetch_one(conn).await?;
     ctx.send(|b| b.embed(|e| {
-        e.title(format!("@{}'s stats", user.tag()))
+        e.title(format!("@{}'s stats", username))
         .field("", &format!(
             "**Total Wins:** {} games
             **Total Lost:** {} games
