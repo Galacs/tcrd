72169d221d1d7744bc8f9693297d8470554a4eb1
Rémi Ait-Younes <remi@ait-younes.fr>
2023-12-02 13:54:52 +0100

    add ability to see other player profile

--- src/commands/profile.rs
+++ src/commands/profile.rs
@@ -5,15 +5,17 @@ use crate::{Context, Error};
 #[poise::command(slash_command, prefix_command)]
 pub async fn profile(
     ctx: Context<'_>,
+    #[description = "User you want to see the profile of"] player: Option<poise::serenity_prelude::User>,
 ) -> Result<(), Error> {
     let conn = &ctx.data().0;
-    let user_id = ctx.author().id.0 as i64;
+    let user = player.unwrap_or(ctx.author().clone());
+    let user_id = user.id.0 as i64;
     crate::create_user::exists_or_create_user(user_id, conn).await?;
     let user_id = user_id.to_string();
     let stats = sqlx::query!("SELECT user_stats.game_won,user_stats.game_lost,balances.balance FROM user_stats INNER JOIN balances ON user_stats.user_id=balances.user_id WHERE user_stats.user_id=$1", user_id).fetch_one(conn).await?;
     let cards_number = sqlx::query!("SELECT count(user_id) AS count FROM users_cards WHERE user_id=$1", user_id).fetch_one(conn).await?;
     ctx.send(|b| b.embed(|e| {
-        e.title(format!("@{}'s stats", ctx.author().tag()))
+        e.title(format!("@{}'s stats", user.tag()))
         .field("", &format!(
             "**Total Wins:** {} games
             **Total Lost:** {} games
