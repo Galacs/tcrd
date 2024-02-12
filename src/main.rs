1015febb2e881b4c22f0ef82edf5b28ae3ffb9c4
Rémi Ait-Younes <remi@ait-younes.fr>
2024-02-12 21:12:50 +0100

    Revert "disabled event pack"
    This reverts commit 081126bd75480258d283e18213ffc06e65c7a9b8.

--- src/main.rs
+++ src/main.rs
@@ -64,7 +64,7 @@ async fn main() -> Result<(), Error> {
 
     let framework = poise::Framework::builder()
         .options(poise::FrameworkOptions {
-            commands: vec![commands::manage::manage(), commands::cards::cards(), commands::balances::hourly(), commands::balances::balance(), commands::balances::daily(), commands::packs::pack(), commands::fight::fight(), commands::profile::profile(), commands::leaderboard::leaderboards(), commands::trade::trade(), commands::help::help(), commands::manage::list()],
+            commands: vec![commands::manage::manage(), commands::cards::cards(), commands::balances::hourly(), commands::balances::balance(), commands::balances::daily(), commands::packs::pack(), commands::packs::eventpack(), commands::fight::fight(), commands::profile::profile(), commands::leaderboard::leaderboards(), commands::trade::trade(), commands::help::help(), commands::manage::list()],
             ..Default::default()
         })
         .token(std::env::var("DISCORD_TOKEN").expect("missing DISCORD_TOKEN"))
